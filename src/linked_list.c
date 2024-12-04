d7f8bc917501491b0bc3d66466ae5b85f5ff2416
Lilian <lilianmon57@gmail.com>
2024-12-04 19:14:43 +0100

    liste chainée

---
+++ src/linked_list.c
@@ -0,0 +1,53 @@
+#include "../include/minishell.h"
+
+void print_list(struct s_shell *current)
+{
+	ft_printf("Liste chainé : ");
+	while (current != NULL)
+	{
+		ft_printf("%s -> ", current->data);
+		current = current->next;
+	}
+	ft_printf("NULL\n");
+}
+
+struct s_shell	*create_node(char *data)
+{
+	struct s_shell	*new_node;
+
+	new_node = malloc(sizeof(struct s_shell));
+	if (new_node == NULL)
+	{
+		write(2, "Error\n", 6);
+		exit(EXIT_FAILURE);
+	}
+	ft_memset(new_node, 0, sizeof(struct s_shell));
+    if (data == NULL)
+	{
+		new_node->data = malloc(256 * sizeof(char));
+		if (new_node->data == NULL)
+		{
+			write(2, "Error\n", 6);
+			free(new_node);
+			exit(EXIT_FAILURE);
+		}
+	}
+    else
+	    new_node->data = data;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	return (new_node);
+}
+
+void insert_head(struct s_shell **head, char *new_data)
+{
+	struct s_shell	*new_node;
+
+	new_node = create_node(new_data);
+	new_node->next = *head;
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+}
