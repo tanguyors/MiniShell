5f0dc8ad0780ec0e80d277eb9aca98a84fd70b79
Lilian <lilianmon57@gmail.com>
2024-12-04 18:53:24 +0100

    ajout de fonctions utilitaire

---
+++ src/is_utils.c
@@ -0,0 +1,36 @@
+#include "../include/minishell.h"
+
+int is_valid_char(int c) 
+{
+    if (is_alnum(c) || c == '_' || c == '-')
+		return (1);
+	return (0);
+}
+
+int is_alpha(int c)
+{
+    if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+        return (1);
+    return (0);
+}
+
+int is_redirect(int c)
+{
+    if (c == '<' || c == '>')
+        return(1);
+    return(0);
+}
+
+int is_alnum(int c)
+{
+	if ((c >= '0' && c <= '9') || (is_alpha(c)))
+		return (1);
+	return (0);
+}
+
+int is_space(int c)
+{
+    if (c == ' ' || c == '\t')
+        return (1);
+    return (0);
+}
