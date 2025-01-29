2a22b209558886220268d0ace57ae80fe453c70d
Lilian Monsat <lmonsat@c2r3s2.42luxembourg.lu>
2025-01-29 19:37:36 +0100

    utils

--- src/utils.c
+++ src/utils.c
@@ -97,3 +97,53 @@ void exit_with_error(const char *str_error, char **array)
         free_array(array);
     exit(EXIT_FAILURE);
 }
+
+size_t ft_strcspn(const char *s, const char *reject)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (s[i])
+    {
+        j = 0;
+        while (reject[j])
+        {
+            if (s[i] == reject[j])
+            {
+                return (i);
+            }
+            j++;
+        }
+        i++;
+    }
+    return (i);
+}
+
+size_t	ft_strspn(const char *s, const char *accept)
+{
+    int i;
+    int j;
+    int check;
+
+    i = 0;
+    while (s[i])
+    {
+        j = 0;
+        check = 0;
+        while (accept[j])
+        {
+            if (s[i] == accept[j])
+            {
+                check = 1;
+            }
+            j++;
+        }
+        if (check == 0)
+        {
+            return (i);
+        }
+        i++;
+    }
+    return (i);
+}
